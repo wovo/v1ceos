e39341b21d28c22f5816f10469f31f59b539e1b8
wovo <wouter@voti.nl>
2018-02-02 14:25:42 +0100

    'work'

---
+++ example-blink/main.cpp
@@ -0,0 +1,16 @@
+#include "mmapgpio.hpp"
+
+const in led_pin = 16;
+
+int main(void){
+   mmapGpio rpiGpio; 
+   rpiGpio.setPinDir( led_pin, mmapGpio::OUTPUT ); 
+   while(1) {
+      rpiGpio.writePinHigh( led_pin );
+      delay (500) ;
+      rpiGpio.writePinLow( led_pin );
+      delay (500) ;
+   }
+
+   return 0;
+}
